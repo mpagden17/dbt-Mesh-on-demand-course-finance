4e96f7e7c8f4dd2df2b453d5b5aaf816f58a2462
Molly Pagden <molly.pagden@dbtlabs.com>
2026-05-22 17:49:59 +0000

    testing mesh

--- models/marts/total_order_revenue.sql
+++ models/marts/total_order_revenue.sql
@@ -1,13 +1,5 @@
 with orders as (
-    select * from fct_orders
-), 
-
-agg as (
-    select 
-        ordered_at,
-        sum(order_total) as order_revenue 
-    from orders 
-    group by 1
+    select * from {{ ref('molly_snowflake_sandbox','fct_orders') }}
 )
 
-select * from agg
+select * from orders
